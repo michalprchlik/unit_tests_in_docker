ddc8e969bc123813e8731ac31f8710201751ae58
michal <michal@localhost>
2021-08-27 16:57:07 +0200

    update test's results

--- bash/test.sh
+++ bash/test.sh
@@ -18,70 +18,70 @@ x=0
 y=0
 expected_result=0
 addition ${x} ${y}
-assert $result $expected_result "${x}+${y}=${expected_result}"
+assert ${result} $expected_result "${x}+${y}=${expected_result}, result was ${result}"
 
 x=1
 y=1
 expected_result=2
 addition ${x} ${y}
-assert $result $expected_result "${x}+${y}=${expected_result}"
+assert ${result} $expected_result "${x}+${y}=${expected_result}, result was ${result}"
 
 x=10
 y=10
 expected_result=20
 addition ${x} ${y}
-assert $result $expected_result "${x}+${y}=${expected_result}"
+assert ${result} $expected_result "${x}+${y}=${expected_result}, result was ${result}"
 
 x=3
 y=1
 expected_result=2
 subtraction ${x} ${y}
-assert $result $expected_result "${x}-${y}=${expected_result}"
+assert ${result} $expected_result "${x}-${y}=${expected_result}, result was ${result}"
 
 x=30
 y=1
 expected_result=29
 subtraction ${x} ${y}
-assert $result $expected_result "${x}-${y}=${expected_result}"
+assert ${result} $expected_result "${x}-${y}=${expected_result}, result was ${result}"
 
 x=0
 y=1
 expected_result=-1
 subtraction ${x} ${y}
-assert $result $expected_result "${x}-${y}=${expected_result}"
+assert ${result} $expected_result "${x}-${y}=${expected_result}, result was ${result}"
 
 x=2
 y=2
 expected_result=4
 multiplication ${x} ${y}
-assert $result $expected_result "${x}*${y}=${expected_result}"
+assert ${result} $expected_result "${x}*${y}=${expected_result}, result was ${result}"
 
 x=100
 y=100
 expected_result=10000
 multiplication ${x} ${y}
-assert $result $expected_result "${x}*${y}=${expected_result}"
+assert ${result} $expected_result "${x}*${y}=${expected_result}, result was ${result}"
 
 x=20
 y=2
 expected_result=40
 multiplication ${x} ${y}
-assert $result $expected_result "${x}*${y}=${expected_result}"
+assert ${result} $expected_result "${x}*${y}=${expected_result}, result was ${result}"
 
 x=10
 y=0
 expected_result=1
 division ${x} ${y}
-assert $? $expected_result "Division by 0 should fail with exit code 1"
+assert $? $expected_result "Division by 0 should fail with exit code 1. Exit code was $?"
 
 x=10
 y=1
 expected_result=10
 division ${x} ${y}
-assert $result $expected_result "${x}/${y}=${expected_result}"
+assert ${result} $expected_result "${x}/${y}=${expected_result}, result was ${result}"
 
 x=4
 y=2
 expected_result=2
 division ${x} ${y}
-assert $result $expected_result "${x}/${y}=${expected_result}"
+assert ${result} $expected_result "${x}/${y}=${expected_result}, result was ${result}"
